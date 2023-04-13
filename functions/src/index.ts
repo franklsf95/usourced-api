ab6d308c9ca64aab7bc697d293f209929e7dee77
Frank Luan <lsf@berkeley.edu>
2023-04-13 06:50:44 +0000

    Remove Airtable; Add triggers for new user creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,87 +1,46 @@
-import axios from "axios";
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Request, Response } from "express";
 import admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-import { isAuthenticated } from "./auth.js";
-import { handleError } from "./res_utils.js";
+const adminApp = admin.initializeApp();
+const auth = adminApp.auth();
+const db = adminApp.firestore();
 
-admin.initializeApp();
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors({ origin: true }));
-
-const AIRTABLE_API_URL = "https://api.airtable.com/v0";
-const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const AIRTABLE_BASE_ID = "appcJSlNIJ6uYs6hY";
-const PROJECTS_TABLE_ID = "tblalL0HVWCQfXVh1";
-
-function makeFilter(linkedCompanies: string[]): string {
-  return (
-    "OR(" +
-    linkedCompanies
-      .map((id) => `FIND("${id}", ARRAYJOIN({ClientID}, ","))`)
-      .join(",") +
-    ")"
-  );
+async function existsCompanyByName(name: string): Promise<boolean> {
+  const snapshot = await db
+    .collection("companies")
+    .where("name", "==", name)
+    .get();
+  return !snapshot.empty;
 }
 
-app.get("/projects", isAuthenticated, async (req: Request, res: Response) => {
-  try {
-    const linkedCompanies = res.locals.linkedCompanies;
-    if (!linkedCompanies) {
-      return res.status(204).send(); // No Content
+async function onCreateOrUpdateUserAccount(
+  snapshot: admin.firestore.DocumentSnapshot,
+  context: functions.EventContext,
+) {
+  const companyName = snapshot.data()?.companyName;
+  const companyExists = await existsCompanyByName(companyName);
+  if (companyExists) {
+    return;
+  }
+  const company = await db.collection("companies").add({ name: companyName });
+  const uid = context.params.uid;
+  const user = await auth.getUser(uid);
+  const claims = user.customClaims;
+  const linkedCompanies = claims?.linkedCompanies || [];
+  linkedCompanies.push(company.id);
+  await auth.setCustomUserClaims(uid, { linkedCompanies });
+}
+
+export const onCreateUserAccount = functions.firestore
+  .document("user_accounts/{uid}")
+  .onCreate(onCreateOrUpdateUserAccount);
+
+export const onUpdateUserAccount = functions.firestore
+  .document("user_accounts/{uid}")
+  .onUpdate(async (change, context) => {
+    const before = change.before.data()?.companyName;
+    const after = change.after.data()?.companyName;
+    if (before !== after) {
+      await onCreateOrUpdateUserAccount(change.after, context);
     }
-    const filterStr = makeFilter(linkedCompanies);
-    const resp = await axios.get(
-      `${AIRTABLE_API_URL}/${AIRTABLE_BASE_ID}/${PROJECTS_TABLE_ID}`,
-      {
-        headers: {
-          Authorization: `Bearer ${AIRTABLE_API_KEY}`,
-        },
-        params: {
-          filterByFormula: filterStr,
-          returnFieldsByFieldId: true,
-          pageSize: 10,
-        },
-      },
-    );
-    const records = resp.data.records;
-    const projects = records.map((record: any) => {
-      return {
-        id: record.id,
-        createdTime: new Date(record.createdTime),
-        inquiryDate: new Date(record.fields["fldfHd0okvI0ic8wP"]),
-        projectImage:
-          "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
-        projectName: record.fields["fldhANhhyIK0Na0cZ"],
-        status: record.fields["fldNJ9qr2UePvmKOP"],
-      };
-    });
-    return res.send({ projects });
-  } catch (err) {
-    return handleError(res, err);
-  }
-});
-
-app.post("/projects", isAuthenticated, async (req: Request, res: Response) => {
-  try {
-    // // make a new record in the projects table
-    // const airbase = Airtable.base(process.env.AIRTABLE_BASE_ID!);
-    // const projectsTable = airbase.table("Projects");
-    // const newRecord = await projectsTable.create({
-    //   "Client Email": req.body.clientEmail,
-    //   // "Created Time": new Date(),
-    // });
-    return res.send({});
-  } catch (err) {
-    return handleError(res, err);
-  }
-});
-
-export const api = functions
-  .runWith({ secrets: ["AIRTABLE_API_KEY", "AIRTABLE_BASE_ID"] })
-  .https.onRequest(app);
+  });
